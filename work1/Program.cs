4862a9fadf666414c9b13319194336f27f04b7fb
Alexander Diakov <alex.dkv@yandex.ru>
2022-09-10 03:22:50 +0300

    added folders and code

---
+++ work1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число ");
+string number = Convert.ToString(Console.ReadLine());
+
+bool CheckPalindrome(string num)
+{
+    string sNum = num.ToString();
+    for (int i = 0; i < sNum.Length; i++)
+        if (sNum[i] != sNum[sNum.Length - 1 - i]) return false;
+
+    return true;
+}
+Console.WriteLine(CheckPalindrome(number));
---
+++ work3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void GetDashbord(int num)
+{
+    for (int count = 1; count <= Math.Abs(num); count++)
+    {
+        Console.WriteLine($"куб числа {count} = {Math.Pow(count, 3)}");
+    }
+}
+GetDashbord(num);
